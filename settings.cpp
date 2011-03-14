485f46dabd7a232d7a721d02534e46c5058ee1b4
unknown <robert.christensen@gmail.com>
2011-03-13 21:19:42 -0600

    found a way to cast characters as QT::keys, so I have changed my stratigy.  Settings now takes a pointer to keys that will be used as key bindings

--- settings.cpp
+++ settings.cpp
@@ -1,19 +1,16 @@
 #include "settings.h"
 #include "ui_settings.h"
 
-Settings::Settings(QWidget *parent) :
+Settings::Settings(KeyBindings *currentSettings, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Settings)
 {
     ui->setupUi(this);
+
+
 }
 
 Settings::~Settings()
 {
     delete ui;
-}
-
-Settings::keyPressEvent(QKeyEvent *event)
-{
-
 }
--- settings.h
+++ settings.h
@@ -7,19 +7,37 @@ namespace Ui {
     class Settings;
 }
 
+// used to comunicate to the settings class what the settings are
+struct KeySettings
+{
+public:
+    char player1_thrust;
+    char player1_left;
+    char player1_right;
+    char player1_shoot;
+    char player1_shield;
+    char player1_brake;
+
+    char player2_thrust;
+    char player2_left;
+    char player2_right;
+    char player2_shoot;
+    char player2_shield;
+    char player2_brake;
+};
+
 class Settings : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit Settings(QWidget *parent = 0);
+    explicit Settings(KeySettings *currentSettings, QWidget *parent = 0);
     ~Settings();
 
 private:
     Ui::Settings *ui;
 
 protected:
-    virtual void keyPressEvent( QKeyEvent *event );
 };
 
 #endif // SETTINGS_H
