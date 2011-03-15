326e8b4c47cdbf13941658fe757fd4f284b901a9
unknown <robert.christensen@gmail.com>
2011-03-14 21:38:06 -0600

    added a few things to the settings window.  I really need more time to figure out a good solution to this problem.

--- settings.cpp
+++ settings.cpp
@@ -1,13 +1,19 @@
 #include "settings.h"
 #include "ui_settings.h"
 
-Settings::Settings(KeyBindings *currentSettings, QWidget *parent) :
+Settings::Settings(KeySettings *currentSettings, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Settings)
 {
     ui->setupUi(this);
 
-
+    // set active settings for player 1
+    ui->Player1_thrust->setText(&currentSettings->player1_thrust);
+    ui->Player1_left->setText(  &currentSettings->player1_left  );
+    ui->Player1_right->setText( &currentSettings->player1_right );
+    ui->Player1_break->setText( &currentSettings->player1_brake );
+    ui->Player1_shield->setText(&currentSettings->player1_shield);
+    ui->Player1_shoot->setText( &currentSettings->player1_shoot);
 }
 
 Settings::~Settings()
